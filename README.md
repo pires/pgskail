9832f5c2663215d7d64624472837b18b0f512df0
Paulo Pires <pjpires@gmail.com>
2015-04-07 14:50:17 +0100

    Implemented basic master election logic (without PostgreSQL control). Updated documentation accordingly.

--- README.md
+++ README.md
@@ -22,14 +22,29 @@ make
 
 ## Run
 
-`pgskail` depends on a `etcd` cluster, so you should have one up and running. For development purposes, you can use `docker` like this
+`pgskail` depends on a `etcd` cluster. For development purposes, you can use `docker` like this
 
 ```
-docker run --net host -e ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379 quay.io/coreos/etcd:v2.0.7
+docker run -d --net host -e ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379 quay.io/coreos/etcd:v2.0.7
 ```
+
+`pgskail` also depends on a `postgresql` instance. For development purposes, you can use `docker` like this
+
+```
+docker run -d --net host --name postgres -e POSTGRES_PASSWORD=teste postgres
+``
 
 Now, you can run `governor` like  
 
 ```
-build/governor --etcd_server http://127.0.0.1:2379
+./build/governor --etcd_server 192.168.59.104 --pg_server 192.168.59.104
 ``
+
+## Flags
+
+* `clean` - Clean-up keystore and start over? (default is **false**)
+* `etcd_host` - Hostname or IP address where Etcd is listening on (default is **127.0.0.1**)
+* `ttl` - Health-check interval in seconds (default is **10**)
+* `pg_host` - Hostname or IP address where PostgreSQL server is listening on (default is **127.0.0.1**)
+* `pg_port` - TCP port where PostgreSQL server is listening on (default is **5432**)
+* `electable` - Is leader electable? (default is **true**)
--- governor.go
+++ governor.go
@@ -2,40 +2,162 @@ package main
 
 import (
 	"flag"
+	"net"
+	"strconv"
 	"time"
 
 	"github.com/coreos/go-etcd/etcd"
 	"github.com/golang/glog"
+	//	"github.com/jackc/pgx"
+)
+
+const (
+	ETCD_PREFIX      = "pgskail"
+	ETCD_TTL_FOREVER = 0
+	KEY_LEADER       = "leader"
 )
 
 type Options struct {
+	CleanKeystore  bool
 	EtcdHost       string
-	HealthCheckTTL time.Duration
-	RunForLeader   bool
+	HealthCheckTTL uint64
+	PgHost         string
+	PgPort         int
+	Electable      bool
 }
 
-var options = &Options{}
+var (
+	options  = &Options{}
+	client   *etcd.Client
+	pgServer string
+	amLeader bool = false
+)
 
-func initFlags() {
+func initializeFlags() {
 	flag.Set("logtostderr", "true")
-	flag.StringVar(&options.EtcdHost, "etcd_server", "http://127.0.0.1:2379", "The etcd server IP:port pair to connect to")
-	flag.DurationVar(&options.HealthCheckTTL, "health_check_ttl", time.Duration(10)*time.Second, "Health-check time-to-live, e.g. 10s. Must confirm to http://golang.org/pkg/time/#ParseDuration")
-	flag.BoolVar(&options.RunForLeader, "run_for_leader", false, "Run for leader?")
+	flag.BoolVar(&options.CleanKeystore, "clean", false, "Clean-up keystore and start over?")
+	flag.StringVar(&options.EtcdHost, "etcd_host", "127.0.0.1", "Hostname or IP address where Etcd is listening on")
+	flag.Uint64Var(&options.HealthCheckTTL, "ttl", 10, "Health-check interval in seconds")
+	flag.StringVar(&options.PgHost, "pg_host", "127.0.0.1", "Hostname or IP address where PostgreSQL server is listening on")
+	flag.IntVar(&options.PgPort, "pg_port", 5432, "TCP port where PostgreSQL server is listening on")
+	flag.BoolVar(&options.Electable, "electable", true, "Is leader electable?")
 	flag.Parse()
 }
 
-const ETCD_PREFIX = "pgskail/governor"
+func GetOptions() Options {
+	return *options
+}
+
+/**
+ * Governor function responsible for handling failover
+ */
+func govern() {
+	// get current leader
+	if leaderResponse, _ := client.Get(KEY_LEADER, false, false); leaderResponse == nil {
+		// no leader. run for it?
+		if options.Electable {
+			glog.Info("No leader was found. Running for leader...")
+			// some competitor may have won already, so fail if it is so
+			if _, err := client.Create(KEY_LEADER, pgServer, options.HealthCheckTTL+5); err == nil {
+				amLeader = true
+				glog.Info("Won leader race")
+			} else {
+				glog.Info("Lost leader race")
+				// look again for the leader
+				govern()
+			}
+		} else {
+			glog.Warning("No leader was found. Retrying in", options.HealthCheckTTL)
+		}
+	} else {
+		if leader := leaderResponse.Node.Value; leader == pgServer {
+			client.Update(KEY_LEADER, pgServer, options.HealthCheckTTL+5)
+			// log only if we
+			amPromoted := !amLeader && true
+			if amLeader = true; amPromoted {
+				glog.Info("I'm leader")
+			}
+		} else {
+			amLeader = false
+			glog.Info("Leader is", leader)
+		}
+	}
+}
 
 func main() {
-	initFlags()
-
+	initializeFlags()
 	defer glog.Flush()
 
-	glog.Infoln("pgskail governor")
-	glog.Info("Connecting to ", options.EtcdHost)
-	machines := []string{options.EtcdHost}
-	client := etcd.NewClient(machines)
-	if _, err := client.Set(ETCD_PREFIX, "test", 0); err != nil {
-		glog.Fatalln("There was an error while connecting to etcd at ", options.EtcdHost, "\n  ", err)
+	glog.Info("pgskail governor")
+
+	// validate PostgreSQL
+	if options.PgPort < 1025 || options.PgPort > 65535 {
+		glog.Fatal("Bad --pg_port value: ", options.PgPort)
 	}
+	pgServer = options.PgHost + ":" + strconv.Itoa(options.PgPort)
+	glog.Info("Connecting to PostgreSQL at ", pgServer, "...")
+	if _, err := net.Dial("tcp", pgServer); err != nil {
+		glog.Error("There was an error while connecting to PostgreSQL")
+		glog.Fatal(err)
+	}
+
+	// validate Etcd
+	etcdServer := options.EtcdHost + ":2379"
+	glog.Info("Connecting to Etcd at ", etcdServer, "...")
+	machines := []string{"http://" + etcdServer}
+	client = etcd.NewClient(machines)
+	if _, err := net.Dial("tcp", etcdServer); err != nil {
+		glog.Error("There was an error while connecting to Etcd")
+		glog.Fatal(err)
+	}
+
+	// need to clean-up?
+	if options.CleanKeystore {
+		glog.Info("Cleaning-up keystore...")
+		// clean-up directory
+		client.Delete(ETCD_PREFIX, true)
+		// create directory
+		if _, err := client.CreateDir(ETCD_PREFIX, ETCD_TTL_FOREVER); err != nil {
+			glog.Fatal("There was an error while creating", ETCD_PREFIX, " directory in etcd. Code:", getErrorCode(err))
+		}
+		glog.Info("Keystore cleaned-up")
+	}
+
+	// govern once right now
+	govern()
+	// schedule future govern executions
+	stop := schedule(govern, options.HealthCheckTTL)
+	defer close(stop)
+
+	select {}
+}
+
+/**
+ * Return int code from etcd.EtcdError
+ */
+func getErrorCode(err error) int {
+	etcdErr, _ := err.(*etcd.EtcdError)
+	return etcdErr.ErrorCode
+}
+
+/**
+ * Schedule a function to run every _interval_ seconds
+ */
+func schedule(what func(), interval uint64) chan struct{} {
+	ticker := time.NewTicker(time.Duration(interval) * time.Second)
+	stop := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				what()
+			case <-stop:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	return stop
 }
